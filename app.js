4f4d1656dc0ac8b05b1c70d8b12d9028ad6e0eff
ditikos <chibioni@gmail.com>
2015-10-06 18:04:25 +0300

    Main Application 1.0.0

---
+++ app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// Setup Initial
+var http	= require('http'),
+	path	= require('path'),
+	express = require('express'),
+	exphbs	= require('express-handlebars'),
+	morgan	= require('morgan');
+
+// Initialize expressJS
+var app 	= express();
+
+// Setup Morgan - Logger
+app.use(morgan('combined'));
+
+// Express-Handlebars setup
+// app.use('views',path.join(__dirname,'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Static File Enable
+app.use(express.static(path.join(__dirname,'public')));
+
+// Routing
+app.get('/', function(req, res) {
+	res.render('home');
+});
+
+
+// Better usage of app/http
+var server = http.createServer(app);
+server.listen(3000, function() {
+	console.log("Server Started");
+});
